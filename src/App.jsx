e088f580de91679e0cce1dd5146875fcc83807f2
Gabriel Augusto <gabrielaugustoassisnascimento@gmail.com>
2025-12-12 11:29:25 -0300

    Versão teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import { Bingo } from './components/Bingo'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Contact } from './pages/Contact'
+import { About } from './pages/About'
 
 function App() {
-
   return (
-    <>
-    <div className='containerApp'>
-     <Bingo />
-    </div>
-    </>
+   <div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/bingo' element={<Bingo />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+   </div>
   )
 }
 
